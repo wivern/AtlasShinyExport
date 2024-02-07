6b19c9b2e76fdcdc77c43c6ac9afbbd83645c5e0
laacdm <luis.alaniz@gmail.com>
2024-02-06 21:37:54 -0600

    New app with partial funtionality (select cohort only for the momment) to come select analysis and select domain

--- apps/cohortCharacterization/dataPrep.R
+++ apps/cohortCharacterization/dataPrep.R
@@ -2,25 +2,53 @@ library(dplyr)
 library(st)
 library(ggplot2)
 
+
 csv_files <-
   list.files("data", pattern = ".csv", full.names = T)
 
 app_data <-
-  purrr::map(csv_files,
+  purrr::map(
+    csv_files,
     ~ data.table::fread(., data.table = T) %>%     # Reads all csv files
       mutate(`Covariate short name` = as.character(`Covariate short name`)) %>%
       mutate(`Covariate short name` = tolower(`Covariate short name`)) %>%
       mutate(`Covariate short name` = gsub("^ ", "", `Covariate short name`)) %>%
       mutate(`Covariate short name` = DescTools::StrCap(`Covariate short name`)) %>%
-      mutate(`Analysis name` = gsub('([a-z])([A-Z])', '\\1 \\2', `Analysis name`, perl = TRUE)) %>%
-      mutate(Percent = tryCatch(round(Percent, digits = 1), error = function(z) return(NA))) %>% # rounding continuous variables where they exists
-      mutate(Avg = tryCatch(round(Avg, digits = 1), error = function(z) return(NA))) %>%
-      mutate(StdDev = tryCatch(round(StdDev, digits = 1), error = function(z) return(NA))) %>%
-      mutate(`Target percent` = tryCatch(round(`Target percent`, digits = 1), error = function(z) return(NA))) %>%
-      mutate(`Cohort percent` = tryCatch(round(`Cohort percent`, digits = 1), error = function(z) return(NA))) %>%
-      mutate(`Std. Diff Of Mean` = tryCatch(round(`Std. Diff Of Mean`, digits = 1), error = function(z) return(NA))) %>%
-      select(-contains("Value field")) %>%         # Eliminating this columns here 
-      select(-contains("Missing Means Zero")) %>%  # because it was not possible to 
+      mutate(
+        `Analysis name` = gsub('([a-z])([A-Z])', '\\1 \\2', `Analysis name`, perl = TRUE)
+      ) %>%
+      mutate(Percent = tryCatch(
+        round(Percent, digits = 1),
+        error = function(z)
+          return(NA)
+      )) %>% # rounding continuous variables where they exists
+      mutate(Avg = tryCatch(
+        round(Avg, digits = 1),
+        error = function(z)
+          return(NA)
+      )) %>%
+      mutate(StdDev = tryCatch(
+        round(StdDev, digits = 1),
+        error = function(z)
+          return(NA)
+      )) %>%
+      mutate(`Target percent` = tryCatch(
+        round(`Target percent`, digits = 1),
+        error = function(z)
+          return(NA)
+      )) %>%
+      mutate(`Cohort percent` = tryCatch(
+        round(`Cohort percent`, digits = 1),
+        error = function(z)
+          return(NA)
+      )) %>%
+      mutate(`Std. Diff Of Mean` = tryCatch(
+        round(`Std. Diff Of Mean`, digits = 1),
+        error = function(z)
+          return(NA)
+      )) %>%
+      select(-contains("Value field")) %>%         # Eliminating this columns here
+      select(-contains("Missing Means Zero")) %>%  # because it was not possible to
       select(-contains("Analysis ID")) %>%         # do inside the Shiny App
       select(-contains("Strata ID")) %>%
       select(-contains("Cohort ID")) %>%
@@ -28,11 +56,22 @@ app_data <-
       select(-contains("Covariate name"))
   )
 
-empty_columns <- lapply(seq_len(length(app_data)), function(x) colSums(is.na(app_data[[x]]) | app_data[[x]] == "") == nrow(is.na(app_data[[x]]))) # Delete all empty columns
+empty_columns <-
+  lapply(seq_len(length(app_data)), function(x)
+    colSums(is.na(app_data[[x]]) |
+              app_data[[x]] == "") == nrow(is.na(app_data[[x]]))) # Delete all empty columns
 
-app_data <- lapply(seq_len(length(app_data)), function(x) app_data[[x]] %>% purrr::discard(empty_columns[[x]]))
+app_data <-
+  lapply(seq_len(length(app_data)), function(x)
+    app_data[[x]] %>% purrr::discard(empty_columns[[x]]))
 
-app_data <- lapply(seq_len(length(app_data)), function(x) if (!is.null(app_data[[x]]$Avg)) {app_data[[x]] %>% mutate(`Boxplot` =  x) } else {app_data[[x]]}) # Insert plot columns where relevant
+app_data <-
+  lapply(seq_len(length(app_data)), function(x)
+    if (!is.null(app_data[[x]]$Avg)) {
+      app_data[[x]] %>% mutate(`Boxplot` =  x)
+    } else {
+      app_data[[x]]
+    }) # Insert plot columns where relevant
 
 # Graphs for the app
 
@@ -41,24 +80,28 @@ lapply(seq_len(length(app_data)), function(a) {
     output <-
       ggplot(app_data[[a]],
              aes(x = `Analysis name`, fill = `Analysis name`)) +
-      geom_boxplot(aes(
-        ymin = Min,
-        lower = P10,
-        middle = Median,
-        upper = P90,
-        ymax = Max
-      ),
-      stat = "identity",
-      show.legend = FALSE) +
+      geom_boxplot(
+        aes(
+          ymin = Min,
+          lower = P10,
+          middle = Median,
+          upper = P90,
+          ymax = Max
+        ),
+        stat = "identity",
+        show.legend = FALSE
+      ) +
       coord_flip() +
       scale_fill_brewer(palette = "Blues") +
       theme(
         axis.title.y = element_blank(),
         axis.text.y = element_blank(),
-        axis.ticks = element_blank())
+        axis.ticks = element_blank()
+      )
     
     png(sprintf("www/p%s.png", a),
-        width = 100, height = 80)
+        width = 100,
+        height = 80)
     plot(output)
     dev.off()
   }
@@ -71,16 +114,14 @@ lapply(seq_len(length(app_data)), function(a) {
 comparatorCohort <- list()
 targetCohort <- list()
 
-# print(!is.null(app_data[[10]]$`Comparator cohort name`))
-
-# Not working the code below
-for(a in 1:length(app_data)) {
+for (a in 1:length(app_data)) {
   if (!is.null(app_data[[a]]$`Comparator cohort name`)) {
     comparatorCohort[[a]] <- app_data[[a]]
-    }
+  }
   else {
     targetCohort[[a]] <- app_data[[a]]
   }
 }
 
-comparatorCohort <- Filter(Negate(is.null), comparatorCohort) # Filter nulls from list
+comparatorCohort <-
+  Filter(Negate(is.null), comparatorCohort) # Filter nulls from list
--- apps/cohortCharacterization/server.R
+++ apps/cohortCharacterization/server.R
@@ -1,93 +1,67 @@
 library(shiny)
-library(dplyr)
 library(reactable)
-library(dplyr)
 source("dataPrep.R")
 
 
 server <- function(input, output, session) {
-  cohort <- reactive({
-    input$cohort
-  })
+  T1 <- list()
+  T2 <- list()
   
-  dataSet <- eventReactive(input$Submit, {
-    if (cohort() == "targetCohort") {
-      return(targetCohort)
-    }
-    else {
-      return(comparatorCohort)
-    }
-  })
   
-  # cohort <- reactive({
-  #   cohort <- input$cohort})
-  #
-  # if (reactive({cohort() == "targetCohort"})) {
-  #   dataSet <- targetCohort
-  # }
-  # else {
-  #   dataSet <- comparatorCohort
-  # }
-  
-  ### Start Debugging Code.
-  
-  output$oid2 <- renderPrint({
-    if (cohort() == "tagetCohort") {
-      paste("You entered: ", cohort())
-    } else {
-      paste("You entered: ", cohort())
-    }
-  })
-  
-  ### End Debugging Code.
-  
-  dataList = list()
-  
-  renderedDataList <-
-    renderReactable({for(a in 1:length(dataSet())) {
-          if (is.null(dataSet()[[a]]$Avg)) {
-            dataList[[paste0("table ", a)]] <-
-              reactable(dataSet()[[a]],
-              sortable = TRUE,
-              showSortable = TRUE,
-              theme = reactableTheme(color = "hsl(0, 0%, 0%)"),
-              showPageSizeOptions = TRUE,
-              pageSizeOptions = c(10, 15, 20),
-              defaultPageSize = 15,
-              style = list(maxWidth = 1600, maxHeight = 900)
-            )
-          }
-          else {
-            dataList[[paste0("table ", a)]] <-
-              reactable(dataSet()[[a]],
-              sortable = TRUE,
-              showSortable = FALSE,
-              theme = reactableTheme(color = "hsl(0, 0%, 0%)"),
-              showPageSizeOptions = TRUE,
-              pageSizeOptions = c(10, 15, 20),
-              defaultPageSize = 15,
-              style = list(maxWidth = 1600, maxHeight = 900),
-              columns = list(Boxplot = colDef(
+  output$tables <- renderUI({
+    if (input$cohort == "targetCohort")
+    {
+      lapply(seq_len(length(targetCohort)), function(x) {
+        if (!is.null(targetCohort[[x]]$Avg)) {
+          T1[[x]] <- reactable(
+            targetCohort[[x]],
+            sortable = TRUE,
+            showSortable = FALSE,
+            theme = reactableTheme(color = "hsl(0, 0%, 0%)"),
+            showPageSizeOptions = TRUE,
+            pageSizeOptions = c(10, 15, 20),
+            defaultPageSize = 15,
+            style = list(maxWidth = 1600, maxHeight = 900),
+            columns = list(
+              Boxplot = colDef(
                 cell = function(a) {
                   div(class = "plot",
                       img(src = sprintf("p%s.png", a)))
                 },
                 width = 120,
                 align = "center"
-              ))
+              )
             )
-          }
+          )
         }
-    })
-
-# nameList <-
-#   lapply(seq_len(length(renderedDataList())), function(a) {
-#     paste0("table ", a)
-#   })
-
-output$tables <- renderUI({
-  lapply(seq_len(length(renderedDataList())), function(a) {
-    reactableOutput(paste0("table ", a))
+        else
+        {
+          T1[[x]] <- reactable(
+            targetCohort[[x]],
+            sortable = TRUE,
+            showSortable = TRUE,
+            theme = reactableTheme(color = "hsl(0, 0%, 0%)"),
+            showPageSizeOptions = TRUE,
+            pageSizeOptions = c(10, 15, 20),
+            defaultPageSize = 15,
+            style = list(maxWidth = 1600, maxHeight = 900)
+          )
+        }
+      })
+    }
+    else {
+      lapply(seq_len(length(targetCohort)), function(x) {
+        T2[[x]] <- reactable(
+          comparatorCohort[[x]],
+          sortable = TRUE,
+          showSortable = TRUE,
+          theme = reactableTheme(color = "hsl(0, 0%, 0%)"),
+          showPageSizeOptions = TRUE,
+          pageSizeOptions = c(10, 15, 20),
+          defaultPageSize = 15,
+          style = list(maxWidth = 1600, maxHeight = 900)
+        )
+      })
+    }
   })
-})
 }
